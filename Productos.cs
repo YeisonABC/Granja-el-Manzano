dabcfa96e29762abd54c3f294721dc228af662c8
unknown <Sala300@TUNDFPCGE10F399.FormacionSena.RED>
2019-08-06 18:21:51 -0500

    Initial commit

---
+++ Productos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Granja_el_Manzano
+{
+    class Productos
+    {
+
+        #region Atributos
+
+        #endregion
+
+        #region Propiedades
+
+            public string Nombre { get; set; }
+            public int Valor_Unitario { get; set; }
+            public int Cantidad { get; set; }
+            public int Valor_Total{ get; set; }
+        
+
+        #endregion
+
+        #region Constructores
+
+        public Productos()
+        {
+
+
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public void insercion()
+        {
+            Console.WriteLine("Ingresa el Nombre ");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("ingresa el valor unitario");
+            Valor_Unitario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingresa la cantidad");
+            Cantidad = int.Parse(Console.ReadLine());
+
+        }
+
+        public void calcular()
+        {
+            Valor_Total = Valor_Unitario * Cantidad;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"El nombre del producto es: {Nombre}");
+            Console.WriteLine($"El valor unitario es: {Valor_Unitario }");
+            Console.WriteLine($"La cantidad es: {Cantidad }");
+            Console.WriteLine($"El valor total de este producto es:{Valor_Total}");
+        }
+
+
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Granja_el_Manzano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Productos Datos = new Productos();
+            
+
+
+            Datos.insercion();
+            Datos.Imprimir();
+            Datos.calcular();
+
+
+           
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
